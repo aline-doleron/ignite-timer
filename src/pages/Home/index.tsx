a5727093b2ecd765ab95e8399305f2d773d0ba68
aline-doleron <doleron.aline@gmail.com>
2024-01-31 19:28:43 -0300

    Using zod library to validate form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,12 +9,34 @@ import {
   TaskInput,
 } from './styles'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mĩnimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no maximo 60 minutos.'),
+})
+
+interface NewCycleFormData {
+  task: string
+  minutesAmount: number
+}
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
 
-  function handleCreateNewCycle(data: unknown) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
 
   const task = watch('task')
